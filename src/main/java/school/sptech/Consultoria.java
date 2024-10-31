e611c1907db77c8e82836abbf823b5b9986ff4e5
isaqueBS <isaque.santana@sptech.school>
2024-10-31 13:21:40 -0300

    Primeira tentativa

--- src/main/java/school/sptech/Consultoria.java
+++ src/main/java/school/sptech/Consultoria.java
@@ -1,4 +1,62 @@
 package school.sptech;
 
+import school.sptech.especialistas.DesenvolvedorWeb;
+
+import java.util.Comparator;
+import java.util.List;
+
 public class Consultoria {
+
+    private String nome;
+    private Integer vagas;
+    private List<Desenvolvedor> desenvolvedores;
+
+    public Consultoria() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(Integer vagas) {
+        this.vagas = vagas;
+    }
+
+    public Boolean contratar(Desenvolvedor desenvolvedor){
+        if(desenvolvedores.size() < vagas) {
+            desenvolvedores.add(desenvolvedor);
+        }
+        return null;
+    }
+
+    public Double getTotalSalario(){
+        return desenvolvedores.stream().
+                mapToDouble(Desenvolvedor::calcularSalario).
+                sum();
+    }
+
+
+    public Desenvolvedor buscarMenorSalario(){
+        if(desenvolvedores.isEmpty()){
+            return null;
+        }
+        return desenvolvedores.stream().
+                min(Comparator.
+                comparing(Desenvolvedor::calcularSalario)).get();
+    }
+
+    public Boolean contratarFullstack(DesenvolvedorWeb desenvolvedor){
+        if(desenvolvedor.isFullstack() == true){
+            desenvolvedores.add(desenvolvedor);
+        }
+        return false;
+    }
 }
--- src/main/java/school/sptech/especialistas/DesenvolvedorWeb.java
+++ src/main/java/school/sptech/especialistas/DesenvolvedorWeb.java
@@ -1,4 +1,61 @@
 package school.sptech.especialistas;
 
-public class DesenvolvedorWeb {
+import school.sptech.Desenvolvedor;
+
+public class DesenvolvedorWeb extends Desenvolvedor {
+
+    private String backend;
+    private String frontend;
+    private String sgbd;
+    private Integer horasMentoria;
+
+    public String getBackend() {
+        return backend;
+    }
+
+    public void setBackend(String backend) {
+        this.backend = backend;
+    }
+
+    public String getFrontend() {
+        return frontend;
+    }
+
+    public void setFrontend(String frontend) {
+        this.frontend = frontend;
+    }
+
+    public String getSgbd() {
+        return sgbd;
+    }
+
+    public void setSgbd(String sgbd) {
+        this.sgbd = sgbd;
+    }
+
+    public Integer getHorasMentoria() {
+        return horasMentoria;
+    }
+
+    public void setHorasMentoria(Integer horasMentoria) {
+        this.horasMentoria = horasMentoria;
+    }
+
+    public Double calcularSalario(){
+        return super.calcularSalario() + (horasMentoria * 300);
+    }
+
+    public DesenvolvedorWeb() {
+    }
+
+    public Boolean isFullstack(){
+        if (getBackend() != null && getFrontend() != null && getSgbd() != null){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
 }
